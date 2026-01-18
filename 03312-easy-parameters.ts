6d01b08eb8c5ebc6c2281a37ebd298ff4d723e75
Shreesha41223 <shreeshaacharya41223@gmail.com>
2026-01-18 21:52:17 +0530

    completed easy types

---
+++ 03312-easy-parameters.ts
@@ -0,0 +1,16 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+function foo(arg1: string, arg2: number): void {}
+function bar(arg1: boolean, arg2: { a: 'A' }): void {}
+function baz(): void {}
+
+type cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>,
+]
+
+
+// ============= Your Code Here =============
+type MyParameters<T extends (...args: any[]) => unknown> = T extends (...args: infer ArgType) => unknown ? ArgType : never;
